a8a2a59ef5273eb53ac16295e30c7a95f42e3a78
Brahian Araya A <brah.araya.a@gmail.com>
2025-01-05 22:32:14 -0300

    add types and remove playlist url param from video url

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
+from typing import Dict, Any
 import yt_dlp
 
-def download_mp3(url):
-    options = {
+PLAYLIST_URL_PARAM = '&list'
+
+def download_mp3(url: str) -> None:
+    sanitized_url = remove_playlist_param(url)
+    options: Dict[str, Any] = {
         'format': 'bestaudio/best',
         'extractaudio': True,
         'audioformat': 'mp3',
@@ -14,16 +18,17 @@ def download_mp3(url):
             }
         ],
     }
-
     with yt_dlp.YoutubeDL(options) as ydl:
         try:
-            print(f"Downloading audio file from video: {url}")
-            ydl.download([url])
+            print(f"Downloading audio file from video: {sanitized_url}")
+            ydl.download([sanitized_url])
             print("Download completed.")
         except Exception as e:
             print(f"Error downloading audio file: {e}")
 
+def remove_playlist_param(url: str) -> str:
+    return url.split(PLAYLIST_URL_PARAM)[0] if PLAYLIST_URL_PARAM in url else url
 
-video_url = input("Paste the video URL: ")
-
-download_mp3(video_url)
+if __name__ == "__main__":
+    video_url: str = input("Paste the video URL: ")
+    download_mp3(video_url)
